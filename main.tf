b415a0b36e305c8eedbce9c0f315b091310a8bbf
ritishreddy <37667025+ritishreddy@users.noreply.github.com>
2025-10-23 10:47:11 +1100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,130 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.100.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# ---------------------------
+# Resource Group
+# ---------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-tomcat"
+  location = "Australia Central"
+}
+
+# ---------------------------
+# Virtual Network & Subnet
+# ---------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-tomcat"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-tomcat"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# ---------------------------
+# Network Security Group
+# ---------------------------
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-tomcat"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "22"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "8080"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
+}
+
+# ---------------------------
+# Network Interfaces
+# ---------------------------
+resource "azurerm_network_interface" "nic" {
+  count               = 2
+  name                = "nic-tomcat-${count.index + 1}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig-tomcat"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    Name = "tomcatservers"
+  }
+}
+
+# ---------------------------
+# Virtual Machines
+# ---------------------------
+resource "azurerm_linux_virtual_machine" "vm" {
+  count               = 2
+  name                = "tomcat-vm-${count.index + 1}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic[count.index].id
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    name              = "osdisk-tomcat-${count.index + 1}"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  tags = {
+    Name = "tomcatservers"
+    Role = "Tomcat"
+  }
+}
